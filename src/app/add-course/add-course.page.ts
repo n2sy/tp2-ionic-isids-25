554c6b6b6ee88f5bbb70d1f7e585e302e851880e
n2sy <nidhal.jelassi@fsegt.utm.tn>
2025-12-09 18:01:12 +0100

    4th step

--- src/app/add-course/add-course.page.ts
+++ src/app/add-course/add-course.page.ts
@@ -1,16 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-course',
   templateUrl: './add-course.page.html',
   styleUrls: ['./add-course.page.scss'],
-  standalone : false
+  standalone: false,
 })
 export class AddCoursePage implements OnInit {
+  tabKeywords = [];
+  constructor(private toastCtrl: ToastController) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  addKeyword(newKeyWordInput) {
+    
+    if (this.tabKeywords.indexOf(newKeyWordInput.value) == -1) {
+      this.tabKeywords.push(newKeyWordInput.value);
+    } else {
+        this.presentToast();
+    }
+    newKeyWordInput.value = '';
+  }
+  
+  deleteKeyword(kwToDelete) {
+    let i = this.tabKeywords.indexOf(kwToDelete);
+    this.tabKeywords.splice(i, 1);
   }
 
+  ngOnInit() {}
+
+  async presentToast() {
+    const toast = await this.toastCtrl.create({
+      message: 'Mot-clé existant',
+      duration: 1500,
+      position: 'bottom',
+    });
+
+    await toast.present();
+  }
+
+  submitHandler(f: NgForm) {
+    console.log(f);
+  }
 }
